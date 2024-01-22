b60094527b8118a6fd779348f2f50414a5a38fab
Stan97-2 <stan.dellon972@gmail.com>
2024-01-22 14:30:49 +0100

    Initial Commit

---
+++ SwiftTechToTechProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftTechToTechProject
+//
+//  Created by goldorak on 22/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftTechToTechProject/SwiftTechToTechProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTechToTechProjectApp.swift
+//  SwiftTechToTechProject
+//
+//  Created by goldorak on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTechToTechProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
